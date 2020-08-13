610deda95a364480519cb0f9d98141b74304fc53
karpenko.a <workartyom3301@gmail.com>
2020-08-13 14:35:06 +0300

    Add project files.

---
+++ DateQRScanner/DateQRScanner/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DateQRScanner.Models;
+using DateQRScanner.ViewModels;
+
+namespace DateQRScanner.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
